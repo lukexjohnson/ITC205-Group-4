079b201635a99b9d575da67834f230413104491d
Paul <paulemills1966@gmail.com>
2018-07-29 12:18:56 +1000

    updated PayFineUI file for review

--- ITC205_BaseLine_Code/src/PayFineUI.java
+++ ITC205_BaseLine_Code/src/PayFineUI.java
@@ -3,90 +3,89 @@ import java.util.Scanner;
 
 public class PayFineUI {
 
-  public static enum UI_STATE { INITIALISED, READY, PAYING, COMPLETED, CANCELLED };
+	public static enum UI_STATE { INITIALISED, READY, PAYING, COMPLETED, CANCELLED };
 
 	private PayFineControl control;
 	private Scanner input;
 	private UI_STATE state;
 
-
 	public PayFineUI(PayFineControl control) {
-	  this.control = control;
-	  input = new Scanner(System.in);
-	  state = UI_STATE.INITIALISED;
-	  control.setUI(this);
+		this.control = control;
+		input = new Scanner(System.in);
+		state = UI_STATE.INITIALISED;
+		control.setUI(this);
 	}
 
 	public void setState(UI_STATE state) {
-	  this.state = state;
+		this.state = state;
 	}
 
-    public void run() {
-	  output("Pay Fine Use Case UI\n");
+	public void run() {
+		output("Pay Fine Use Case UI\n");
 
-	  while (true) {
+		while (true) {
 
-		switch (state) {
+			switch (state) {
 
-		case READY:
-			String memStr = input("Swipe member card (press <enter> to cancel): ");
-			if (memStr.length() == 0) {
-			  control.cancel();
-			  break;
-			  }
-			try {
-			  int memberId = Integer.valueOf(memStr).intValue();
-			  control.cardSwiped(memberId);
-			    }
-			catch (NumberFormatException e) {
-			  output("Invalid memberId");
+			case READY:
+				String memberStr = input("Swipe member card (press <enter> to cancel): ");
+				if (memberStr.length() == 0) {
+					control.cancel();
+					break;
+				}
+				try {
+					int memberId = Integer.valueOf(memberStr).intValue();
+					control.cardSwiped(memberId);
+				}
+				catch (NumberFormatException e) {
+					output("Invalid memberId");
 				}
 				break;
 
 			case PAYING:
-			  double amount = 0;
-			  String amtStr = input("Enter amount (<Enter> cancels) : ");
-			  if (amtStr.length() == 0) {
-			    control.cancel();
-				break;
+				double amount = 0;
+				String amountStr = input("Enter amount (<Enter> cancels) : ");
+				if (amountStr.length() == 0) {
+					control.cancel();
+					break;
 				}
 				try {
-				  amount = Double.valueOf(amtStr).doubleValue();
+					amount = Double.valueOf(amountStr).doubleValue();
 				}
-				catch (NumberFormatException e) { 
-				}
+				catch (NumberFormatException e) {}
 				if (amount <= 0) {
-				  output("Amount must be positive");
-				  break;
+					output("Amount must be positive");
+					break;
 				}
 				control.payFine(amount);
 				break;
 
 			case CANCELLED:
-			  output("Pay Fine process cancelled");
-			  break;
+				output("Pay Fine process cancelled");
+				break;
 
 			case COMPLETED:
-			  output("Pay Fine process complete");
-			  break;
+				output("Pay Fine process complete");
+				break;
 
 			default:
-			  output("Unhandled state");
-			  throw new RuntimeException("FixBookUI : unhandled state :" + state);
+				output("Unhandled state");
+				throw new RuntimeException("FixBookUI : unhandled state :" + state);
 			}
 		}
 	}
 
-    private String input(String prompt) {
-	  System.out.print(prompt);
-	  return input.nextLine();
+	private String input(String prompt) {
+		System.out.print(prompt);
+		return input.nextLine();
 	}
 
 	private void output(Object object) {
-	  System.out.println(object);
+		System.out.println(object);
 	}
 
 	public void display(Object object) {
-	  output(object);
+		output(object);
 	}
 }
+
