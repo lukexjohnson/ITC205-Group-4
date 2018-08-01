56bccbf49b0255dbe207ae02da0c8741c502dbd0
Benn <benncurby@gmail.com>
2018-08-01 21:14:04 +1000

    changed UI_STATE -> UiState within BorrowBookControl.java

--- ITC205_BaseLine_Code/src/BorrowBookControl.java
+++ ITC205_BaseLine_Code/src/BorrowBookControl.java
@@ -26,7 +26,7 @@ public class BorrowBookControl {
 			throw new RuntimeException("BorrowBookControl: cannot call setUI except in INITIALISED state");
 			
 		this.ui = ui;
-		ui.setState(BorrowBookUi.UI_STATE.READY);
+		ui.setState(BorrowBookUi.UiState.READY);
 		state = CONTROL_STATE.READY;		
 	}
 
@@ -42,12 +42,12 @@ public class BorrowBookControl {
 		}
 		if (L.memberCanBorrow(M)) {
 			PENDING = new ArrayList<>();
-			ui.setState(BorrowBookUi.UI_STATE.SCANNING);
+			ui.setState(BorrowBookUi.UiState.SCANNING);
 			state = CONTROL_STATE.SCANNING; }
 		else 
 		{
 			ui.display("Member cannot borrow at this time");
-			ui.setState(BorrowBookUi.UI_STATE.RESTRICTED); }}
+			ui.setState(BorrowBookUi.UiState.RESTRICTED); }}
 	
 	
 	public void Scanned(int bookId) {
@@ -85,7 +85,7 @@ public class BorrowBookControl {
 				ui.display(b.toString());
 			}
 			COMPLETED = new ArrayList<loan>();
-			ui.setState(BorrowBookUi.UI_STATE.FINALISING);
+			ui.setState(BorrowBookUi.UiState.FINALISING);
 			state = CONTROL_STATE.FINALISING;
 		}
 	}
@@ -103,13 +103,13 @@ public class BorrowBookControl {
 		for (loan loan : COMPLETED) {
 			ui.display(loan.toString());
 		}
-		ui.setState(BorrowBookUi.UI_STATE.COMPLETED);
+		ui.setState(BorrowBookUi.UiState.COMPLETED);
 		state = CONTROL_STATE.COMPLETED;
 	}
 
 	
 	public void cancel() {
-		ui.setState(BorrowBookUi.UI_STATE.CANCELLED);
+		ui.setState(BorrowBookUi.UiState.CANCELLED);
 		state = CONTROL_STATE.CANCELLED;
 	}
 	
