51db689f9289461667cb2407abc1a7d03211c081
Paul <paulemills1966@gmail.com>
2018-08-04 14:22:52 +1000

    Updated file as per review - fixed D1 -D5

--- ITC205_BaseLine_Code/src/PayFineControl.java
+++ ITC205_BaseLine_Code/src/PayFineControl.java
@@ -1,10 +1,11 @@
 public class PayFineControl {
 	
-	private PayFineUI ui;
 	private enum ControlState { INITIALISED, READY, PAYING, COMPLETED, CANCELLED };
+	
+	private PayFineUI ui;
 	private ControlState state;
 	private library library;
-	private member member;;
+	private member member;
 
 
 	public PayFineControl() {
@@ -15,7 +16,8 @@ public class PayFineControl {
 	
 	public void setUI(PayFineUI ui) {
 		if (!state.equals(ControlState.INITIALISED)) {
-			throw new RuntimeException("PayFineControl: cannot call setUI except in INITIALISED state");
+			throw new RuntimeException("PayFineControl:"
+					+ " cannot call setUI except in INITIALISED state");
 		}	
 		this.ui = ui;
 		ui.setState(PayFineUI.UIState.READY);
@@ -25,7 +27,8 @@ public class PayFineControl {
 	
 	public void cardSwiped(int memberId) {
 		if (!state.equals(ControlState.READY)) {
-			throw new RuntimeException("PayFineControl: cannot call cardSwiped except in READY state");
+			throw new RuntimeException("PayFineControl:"
+					+ " cannot call cardSwiped except in READY state");
 		}	
 		member = library.getMember(memberId);
 		
@@ -47,7 +50,8 @@ public class PayFineControl {
 	
 	public double payFine(double amount) {
 		if (!state.equals(ControlState.PAYING)) {
-			throw new RuntimeException("PayFineControl: cannot call payFine except in PAYING state");
+			throw new RuntimeException("PayFineControl:"
+					+ " cannot call payFine except in PAYING state");
 		}	
 		double change = member.payFine(amount);
 		if (change > 0) {
