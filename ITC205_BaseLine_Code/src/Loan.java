e7f1ce1102042f92c602b2c06b556d2bd28ccd9e
Paul <paulemills1966@gmail.com>
2018-08-07 21:42:22 +1000

    Applied changes from review

--- ITC205_BaseLine_Code/src/Loan.java
+++ ITC205_BaseLine_Code/src/Loan.java
@@ -7,25 +7,25 @@ public class Loan implements Serializable {
 	
 	public static enum LoanState { CURRENT, OVER_DUE, DISCHARGED };
 	
-	private int ID;
-	private book B;
-	private member M;
-	private Date D;
+	private int loanID;
+	private book book;
+	private member member;
+	private Date dueDate;
 	private LoanState state;
 
 	
 	public Loan(int loanId, book book, member member, Date dueDate) {
-		this.ID = loanId;
-		this.B = book;
-		this.M = member;
-		this.D = dueDate;
+		this.loanID = loanId;
+		this.book = book;
+		this.member = member;
+		this.dueDate = dueDate;
 		this.state = LoanState.CURRENT;
 	}
 
 	
 	public void checkOverDue() {
 		if (state == LoanState.CURRENT &&
-			Calendar.getInstance().Date().after(D)) {
+			Calendar.getInstance().Date().after(dueDate)) {
 			this.state = LoanState.OVER_DUE;			
 		}
 	}
@@ -37,37 +37,37 @@ public class Loan implements Serializable {
 
 	
 	public Integer getId() {
-		return ID;
+		return loanID;
 	}
 
 
 	public Date getDueDate() {
-		return D;
+		return dueDate;
 	}
 	
 	
 	public String toString() {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
-		StringBuilder sb = new StringBuilder();
-		sb.append("Loan:  ").append(ID).append("\n")
-		  .append("  Borrower ").append(M.getId()).append(" : ")
-		  .append(M.getLastName()).append(", ").append(M.getFirstName()).append("\n")
-		  .append("  Book ").append(B.ID()).append(" : " )
-		  .append(B.Title()).append("\n")
-		  .append("  DueDate: ").append(sdf.format(D)).append("\n")
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("Loan:  ").append(loanID).append("\n")
+		  .append("  Borrower ").append(member.getId()).append(" : ")
+		  .append(member.getLastName()).append(", ").append(member.getFirstName()).append("\n")
+		  .append("  Book ").append(book.ID()).append(" : " )
+		  .append(book.Title()).append("\n")
+		  .append("  DueDate: ").append(simpleDateFormat.format(dueDate)).append("\n")
 		  .append("  State: ").append(state);		
-		return sb.toString();
+		return stringBuilder.toString();
 	}
 
 
 	public member Member() {
-		return M;
+		return member;
 	}
 
 
 	public book Book() {
-		return B;
+		return book;
 	}
 
 
