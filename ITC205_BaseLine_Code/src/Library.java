a59848d27e19296a6d5460a3ec17bd2f251c0e57
Benn <benncurby@gmail.com>
2018-08-12 13:39:49 +1000

    changed int BID -> bookID

--- ITC205_BaseLine_Code/src/Library.java
+++ ITC205_BaseLine_Code/src/Library.java
@@ -24,7 +24,7 @@ public class Library implements Serializable {
     private static final double DAMAGE_FEE = 2.0;
 
     private static Library self;
-    private int BID;
+    private int bookId;
     private int MID;
     private int LID;
     private Date loadDate;
@@ -42,7 +42,7 @@ public class Library implements Serializable {
         loans = new HashMap<>();
         currentLoans = new HashMap<>();
         damagedBooks = new HashMap<>();
-        BID = 1;
+        bookId = 1;
         MID = 1;
         LID = 1;
     }
@@ -82,7 +82,7 @@ public class Library implements Serializable {
 
 
     public int BookID() {
-        return BID;
+        return bookId;
     }
 
 
@@ -92,7 +92,7 @@ public class Library implements Serializable {
 
 
     private int nextBID() {
-        return BID++;
+        return bookId++;
     }
 
 
