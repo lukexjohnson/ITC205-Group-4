95505a7ff14fc3aabe4b460bb0c7901840be3655
lukej <lukexjohnson@outlook.com>
2018-08-14 21:58:46 +1000

    removed additional tabs in lines as per review

--- ITC205_BaseLine_Code/src/Main.java
+++ ITC205_BaseLine_Code/src/Main.java
@@ -9,11 +9,11 @@ public class Main {
 	private static String libraryMenu;
 	private static Calendar calendar;
 	private static SimpleDateFormat simpleDateFormat;
-	
-	
+
+
 	private static String getMenu() {
 	    StringBuilder buildString = new StringBuilder();
-		
+
 	    buildString.append("\nLibrary Main Menu\n\n")
 		    .append("  M  : add member\n")
 		    .append("  LM : list members\n")
@@ -38,12 +38,12 @@ public class Main {
 
 
 	public static void main(String[] args) {		
-	    try {			
+	    try {
 	        takeInput = new Scanner(System.in);
 	        library = library.INSTANCE(); // change library.INSTANCE to Library.INSTANCE
 	        calendar = Calendar.getInstance();
 	        simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
-	
+
 	        for (member m : library.Members()) {
 	            output(m);
 			}
@@ -51,77 +51,77 @@ public class Main {
 	        for (Book b : library.Books()) {
 	            output(b);
 			}
-					
+
 	        libraryMenu = getMenu();
-			
+
 	        boolean e = false;
-			
+
 	        while (!e) {
-				
+
 	            output("\n" + simpleDateFormat.format(calendar.Date()));
 	            String c = input(libraryMenu);
-				
+
 	            switch (c.toUpperCase()) {
-				
+
 	            case "M": 
 	                addMember();
 	                break;
-					
+
 			    case "LM": 
 			        listMembers();
 			        break;
-					
+
 			    case "B": 
 				    addBook();
 				    break;
-					
+
 			    case "LB": 
 			        listBooks();
 			        break;
-					
+
 			    case "FB": 
 			        fixBooks();
 			        break;
-					
+
 			    case "L": 
 			        borrowBook();
 	                break;
-					
+
 	            case "R": 
 	                returnBook();
 	                break;
-					
+
 				case "LL": 
 				    listCurrentLoans();
 				    break;
-					
+
 				case "P": 
 				    payFine();
 				    break;
-					
+
 				case "T": 
 				    incrementDate();
 				    break;
-					
+
 				case "Q": 
 				    e = true;
 				    break;
-					
+
 				default: 
 				    output("\nInvalid option\n");
 				    break;
 			    }
-				
+
 	            library.SAVE();
-	        }			
+	        }
 		} catch (RuntimeException e) {
 		    output(e);
-		}		
+		}
 	    output("\nEnded\n");
-	}	
+	}
 
 		private static void payFine() {
-	    new PayFineUI(new PayFineControl()).run();		
+	    new PayFineUI(new PayFineControl()).run();
 	}
 
 
@@ -129,7 +129,7 @@ public class Main {
 	    output("");
 	    for (Loan loan : library.CurrentLoans()) {
 	        output(loan + "\n");
-	    }		
+	    }
 	}
 
 
@@ -137,7 +137,7 @@ public class Main {
 	    output("");
 	    for (Book book : library.Books()) {
 	        output(book + "\n");
-	    }		
+	    }
 	}
 
 
@@ -145,22 +145,22 @@ public class Main {
 	    output("");
 	    for (member member : library.Members()) {
 	        output(member + "\n");
-	    }		
+	    }
 	}
 
 
 	private static void borrowBook() {
-	    new BorrowBookUI(new BorrowBookControl()).run();		
+	    new BorrowBookUI(new BorrowBookControl()).run();
 	}
 
 
 	private static void returnBook() {
-	    new ReturnBookUI(new ReturnBookControl()).run();		
+	    new ReturnBookUI(new ReturnBookControl()).run();
 	}
 
 
 	private static void fixBooks() {
-	    new FixBookUI(new FixBookControl()).run();		
+	    new FixBookUI(new FixBookControl()).run();
 	}
 
 
@@ -170,22 +170,22 @@ public class Main {
 		    calendar.incrementDate(days);
 		    library.checkCurrentLoans();
 		    output(simpleDateFormat.format(calendar.Date()));
-		    
+
 		} catch (NumberFormatException e) {
 		    output("\nInvalid number of days\n");
 		}
 	}
 
 
-	private static void addBook() {		
+	private static void addBook() {
 	    String author = input("Enter author: ");
 	    String title  = input("Enter title: ");
 	    String callNo = input("Enter call number: ");
 	    Book book = library.Add_book(author, title, callNo);
-	    output("\n" + book + "\n");		
+	    output("\n" + book + "\n");
 	}
 
-	
+
 	private static void addMember() {
 	    try {
 		    String lastName = input("Enter last name: ");
@@ -194,10 +194,10 @@ public class Main {
 		    int phoneNo = Integer.valueOf(input("Enter phone number: ")).intValue();
 		    member member = library.Add_mem(lastName, firstName, email, phoneNo);
 		    output("\n" + member + "\n");
-			
+
 		} catch (NumberFormatException e) {
 		    output("\nInvalid phone number\n");
-		}		
+		}
 	}
 
 
@@ -205,11 +205,11 @@ public class Main {
 	    System.out.print(prompt);
 	    return takeInput.nextLine();
 	}
-	
-		
+
+
 	private static void output(Object outputObject) {
 	    System.out.println(outputObject);
 	}
 
-	
+
 }
